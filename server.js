8ba8266177eecdcd885b9d6015cd96959e87e03d
Arturo Ayon <arturo.al@hotmail.com>
2019-09-09 21:07:49 -0600

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 //Connect Database
@@ -7,7 +8,6 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/clients', require('./routes/api/clients'));
@@ -15,16 +15,15 @@ app.use('/api/products', require('./routes/api/products'));
 app.use('/api/configurations', require('./routes/api/configurations'));
 app.use('/api/sales', require('./routes/api/sales'));
 
-//app.use('/api/config', require('./routes/api/config'));
-/*app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-*/
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
